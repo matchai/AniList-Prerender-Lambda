63d6dd0a988f0545570c5263244b4e5270e99d10
Matan Kushner <hello@matchai.me>
2018-08-04 03:11:37 -0400

    IT WORKS!

---
+++ src/ssr.js
@@ -0,0 +1,70 @@
+const request = require('superagent');
+const launchChrome = require('@serverless-chrome/lambda');
+const puppeteer = require('puppeteer');
+
+async function getChrome() {
+  const chrome = await launchChrome();
+
+  const response = await request
+    .get(`${chrome.url}/json/version`)
+    .set('Content-Type', 'application/json');
+
+  const debuggerUrl = response.body.webSocketDebuggerUrl;
+
+  return {
+    debuggerUrl,
+    instance: chrome,
+  };
+}
+
+async function render(url) {
+  const chrome = await getChrome();
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: chrome.debuggerUrl,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'networkidle0' });
+
+  let html = '[NotFound] Page not found';
+  if (page.url() !== 'https://anilist.co/404') {
+    html = await page.content();
+  }
+
+  await browser.close();
+  setTimeout(() => chrome.instance.kill(), 0);
+  return html;
+}
+
+function log(...stuffToLog) {
+  if (process.env.LOGGING) {
+    console.log(...stuffToLog);
+  }
+}
+
+module.exports.handler = async function handler(event, context, callback) {
+  const { url } = event.queryStringParameters || {};
+
+  let data;
+
+  log('Prerendering', url);
+
+  const startTime = Date.now();
+
+  try {
+    data = await render(url);
+  } catch (error) {
+    console.error('Error capturing screenshot for', url, error);
+    return callback(error);
+  }
+
+  log(`Chromium took ${Date.now() - startTime}ms to load URL and capture screenshot.`);
+
+  return callback(null, {
+    statusCode: 200,
+    body: data,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  });
+};
